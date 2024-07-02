70027d5013373355b6778f6b8b6565d448aa14ac
Gene Hyde <Gene@HydeEnterprises.com>
2024-07-01 20:43:13 -0700

    refactor

--- built/savedBoardsHtmlGenerator.js
+++ built/savedBoardsHtmlGenerator.js
@@ -1,48 +1,44 @@
+function deriveUnorderedListElement(listItemContents) {
+    const listItems = listItemContents.map(function (listItemContent) {
+        const rc = document.createElement('li');
+        rc.appendChild(listItemContent);
+        return rc;
+    });
+    const rc = document.createElement('ul');
+    rc.append(...listItems);
+    return rc;
+}
 function deriveHeaderElement() {
     const headerTextNode = document.createTextNode('Saved boards');
     const headerElement = document.createElement('h3');
     headerElement.appendChild(headerTextNode);
     return headerElement;
 }
-function deriveCellListItemElement(liveCell) {
-    const cellListItemTextNode = document.createTextNode("row: " + liveCell.rowIndex + " column: " + liveCell.columnIndex);
-    const cellListItemElement = document.createElement('li');
-    cellListItemElement.appendChild(cellListItemTextNode);
-    return cellListItemElement;
-}
-function deriveCellsListElement(liveCells) {
-    const cellsListElement = document.createElement('ul');
-    liveCells.forEach(liveCell => {
-        const cellListItemElement = deriveCellListItemElement(liveCell);
-        cellsListElement.appendChild(cellListItemElement);
+function deriveBoardsListElement(boards) {
+    const spanElements = boards.map(function (board) {
+        const spanElement = document.createElement('span');
+        spanElement.append(board.name);
+        const liveCellListItemElements = board.liveCells.map(function (liveCell) {
+            const rc = document.createElement('span');
+            rc.append("row: " + liveCell.rowIndex + " column: " + liveCell.columnIndex);
+            return rc;
+        });
+        const liveCellsListElement = deriveUnorderedListElement(liveCellListItemElements);
+        spanElement.append(liveCellsListElement);
+        return spanElement;
     });
-    return cellsListElement;
-}
-function deriveBoardListItemElement(board) {
-    const boardListitemElement = document.createElement('li');
-    const boardListItemNameTextNode = document.createTextNode(board.name);
-    boardListitemElement.appendChild(boardListItemNameTextNode);
-    const cellsListElement = deriveCellsListElement(board.liveCells);
-    boardListitemElement.appendChild(cellsListElement);
-    return boardListitemElement;
-}
-async function deriveBoardsListElement() {
-    const boardsListElement = document.createElement('ul');
-    const response = await fetch(`/api/boards`);
-    const savedBoardsJson = await response.json();
-    const boards = savedBoardsJson.boards;
-    boards.forEach((board) => {
-        const boardListitemElement = deriveBoardListItemElement(board);
-        boardsListElement.appendChild(boardListitemElement);
-    });
-    return boardsListElement;
+    const rc = deriveUnorderedListElement(spanElements);
+    return rc;
 }
 export function SavedBoardsHtmlGenerator() {
     async function savedBoardsElement() {
         const rc = document.createElement('p');
         const headerElement = deriveHeaderElement();
         rc.appendChild(headerElement);
-        const boardsListElement = await deriveBoardsListElement();
+        const response = await fetch(`/api/boards`);
+        const savedBoardsJson = await response.json();
+        const boards = savedBoardsJson.boards;
+        const boardsListElement = deriveBoardsListElement(boards);
         rc.appendChild(boardsListElement);
         return rc;
     }
--- src/savedBoardsHtmlGenerator.ts
+++ src/savedBoardsHtmlGenerator.ts
@@ -1,6 +1,12 @@
-
-export interface ISavedBoardsHtmlGenerator {
-    savedBoardsElement: Function
+function deriveUnorderedListElement(listItemContents: HTMLElement[]): HTMLUListElement {
+    const listItems: HTMLLIElement[] = listItemContents.map(function(listItemContent: HTMLElement) {
+        const rc: HTMLLIElement = document.createElement('li')
+        rc.appendChild(listItemContent)
+        return rc
+    })
+    const rc: HTMLUListElement = document.createElement('ul')
+    rc.append(...listItems)
+    return rc
 }
 
 function deriveHeaderElement(): HTMLElement {
@@ -20,41 +26,25 @@ interface ISavedBoard {
     liveCells: ICell[]
 }
 
-function deriveCellListItemElement(liveCell: ICell): HTMLLIElement {
-    const cellListItemTextNode = document.createTextNode("row: " + liveCell.rowIndex + " column: " + liveCell.columnIndex)
-    const cellListItemElement = document.createElement('li')
-    cellListItemElement.appendChild(cellListItemTextNode)
-    return cellListItemElement
-}
-
-function deriveCellsListElement(liveCells: ICell[]): HTMLUListElement {
-    const cellsListElement: HTMLUListElement = document.createElement('ul')
-    liveCells.forEach(liveCell => {
-        const cellListItemElement = deriveCellListItemElement(liveCell)
-        cellsListElement.appendChild(cellListItemElement)
-    });
-    return cellsListElement
-}
-
-function deriveBoardListItemElement(board: ISavedBoard): HTMLLIElement {
-    const boardListitemElement = document.createElement('li')
-    const boardListItemNameTextNode = document.createTextNode(board.name)
-    boardListitemElement.appendChild(boardListItemNameTextNode)
-    const cellsListElement: HTMLUListElement = deriveCellsListElement(board.liveCells)
-    boardListitemElement.appendChild(cellsListElement)
-    return boardListitemElement
-}
-
-async function deriveBoardsListElement(): Promise<HTMLUListElement> {
-    const boardsListElement: HTMLUListElement = document.createElement('ul')
-    const response: Response = await fetch(`/api/boards`)
-    const savedBoardsJson = await response.json()
-    const boards = savedBoardsJson.boards
-    boards.forEach((board: ISavedBoard) => {
-        const boardListitemElement = deriveBoardListItemElement(board)
-        boardsListElement.appendChild(boardListitemElement)
+function deriveBoardsListElement(boards: ISavedBoard[]): HTMLUListElement {
+    const spanElements: HTMLSpanElement[] = boards.map(function(board: ISavedBoard): HTMLSpanElement {
+        const spanElement: HTMLSpanElement = document.createElement('span')
+        spanElement.append(board.name)
+        const liveCellListItemElements: HTMLSpanElement[] = board.liveCells.map(function(liveCell): HTMLSpanElement {
+            const rc = document.createElement('span')
+            rc.append("row: " + liveCell.rowIndex + " column: " + liveCell.columnIndex)
+            return rc
+        })
+        const liveCellsListElement: HTMLUListElement = deriveUnorderedListElement(liveCellListItemElements)
+        spanElement.append(liveCellsListElement)
+        return spanElement
     })
-    return boardsListElement
+    const rc: HTMLUListElement = deriveUnorderedListElement(spanElements)
+    return rc
+}
+
+export interface ISavedBoardsHtmlGenerator {
+    savedBoardsElement: Function
 }
 
 export function SavedBoardsHtmlGenerator() : ISavedBoardsHtmlGenerator {
@@ -62,7 +52,12 @@ export function SavedBoardsHtmlGenerator() : ISavedBoardsHtmlGenerator {
         const rc: HTMLParagraphElement = document.createElement('p')
         const headerElement: HTMLElement = deriveHeaderElement()
         rc.appendChild(headerElement)
-        const boardsListElement: HTMLUListElement = await deriveBoardsListElement()
+
+        const response: Response = await fetch(`/api/boards`)
+        const savedBoardsJson = await response.json()
+        const boards: ISavedBoard[] = savedBoardsJson.boards
+
+        const boardsListElement: HTMLUListElement = deriveBoardsListElement(boards)
         rc.appendChild(boardsListElement)
         return rc
     }
